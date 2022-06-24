2f6306597f93a90b7cd93f9a265034b08ed40c31
MekiKry6i <miroslav.pastrakov@gmail.com>
2022-06-25 00:15:02 +0300

    first reducer

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,7 +1,8 @@
 import { createAction , props} from "@ngrx/store";
+import { RegisterUserInterface } from "src/app/shared/types/registerRiquest.interface";
 import { ActionTypes } from "./action-types";
 
 export const registerAction = createAction(
     ActionTypes.REGISTER,
-    props<{ username: string; email:string; password: string;}>()
+    props<{request: RegisterUserInterface }>()
 )
